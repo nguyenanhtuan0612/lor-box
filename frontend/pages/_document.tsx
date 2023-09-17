ec22f8725a4c1a6929ef26c5783b8cc56e168651
tuananhvd1998 <tuananhvd1998@gmail.com>
2023-09-17 13:03:26 +0700

    google analytic

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,15 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP34Y5XK9B"></script>
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KP34Y5XK9B');`}
+        </script>
         <link rel="shortcut icon" href="/logo.ico" />
       </Head>
       <body>
